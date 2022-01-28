10a1fa59a94ae85a0ea3c8ac788c8a84dd0dcae6
Fatu-cham <90718155+Fatu-cham@users.noreply.github.com>
2022-01-28 14:23:22 +0000

    carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,45 @@ import * as React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/layout";
 
+import { Carousel } from 'antd';
+
+function onChange(a, b, c) {
+  console.log(a, b, c);
+}
+
+const contentStyle = {
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
+
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+    <div>
+       <Layout pageTitle="Home Page">
+      <p>WELCOME TO OUR AXOLOTL ADOPTION SITE</p>
     </Layout>
+
+    <Carousel afterChange={onChange}>
+    <div>
+      <h3 style={contentStyle}>1</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>2</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>3</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>4</h3>
+    </div>
+  </Carousel>, mountNode,
+  </div>
+   
+
   );
 };
 
