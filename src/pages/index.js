801a2eb5232845e5e87bb96f3893d87ff07e8efa
scottwilson <swrs@hotmail.co.uk>
2022-01-28 15:09:09 +0000

    got the carousel working

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,46 +2,42 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/layout";
-
-import { Carousel } from 'antd';
+// import  Carousel  from "gatsby-plugin-antd";
+import { Carousel } from "antd";
 
 function onChange(a, b, c) {
   console.log(a, b, c);
 }
-
 const contentStyle = {
-  height: '160px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
+  height: "160px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "#364d79",
 };
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <div>
-       <Layout pageTitle="Home Page">
-      <p>WELCOME TO OUR AXOLOTL ADOPTION SITE</p>
-    </Layout>
-
-    <Carousel afterChange={onChange}>
-    <div>
-      <h3 style={contentStyle}>1</h3>
+      <Layout pageTitle="Home Page">
+        <p>WELCOME TO OUR AXOLOTL ADOPTION SITE</p>
+        <Carousel afterChange={onChange}>
+          <div>
+            <h3 style={contentStyle}>1</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>2</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>3</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>4</h3>
+          </div>
+        </Carousel>
+      </Layout>
     </div>
-    <div>
-      <h3 style={contentStyle}>2</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>3</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>4</h3>
-    </div>
-  </Carousel>, mountNode,
-  </div>
-   
-
   );
 };
 
